89fcfb5cb5146c090441a3260f65bf8c0f22e3ea
ooooMarcinoooo <kucmarcin@o2.pl>
2017-01-21 14:02:52 +0100

    klas

--- softTSP/softTSP/Program.cs
+++ softTSP/softTSP/Program.cs
@@ -16,7 +16,7 @@ namespace softTSP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new TspForm());
         }
     }
 }
